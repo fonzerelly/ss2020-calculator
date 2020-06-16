d7568e302bcf00f9d1043d178fc87726b4750267
fonzerelly <christian.hoerauf@gmail.com>
2020-06-16 09:29:11 +0200

    sets up click dummy

--- src/App.js
+++ src/App.js
@@ -1,24 +1,73 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="calculator">
+        <div className="display">
+          <p className="display__value">0</p>
+        </div>
+        <div className="keypad">
+          <div className="keypad__number">
+            <div className="keypad__key--number">
+              <p className="keypad__key__value">9</p>
+            </div>
+            <div className="keypad__key--number">
+              <p className="keypad__key__value">8</p>
+            </div>
+            <div className="keypad__key--number">
+              <p className="keypad__key__value">7</p>
+            </div>
+            <div className="keypad__key--number">
+              <p className="keypad__key__value">6</p>
+            </div>
+            <div className="keypad__key--number">
+              <p className="keypad__key__value">5</p>
+            </div>
+            <div className="keypad__key--number">
+              <p className="keypad__key__value">4</p>
+            </div>
+            <div className="keypad__key--number">
+              <p className="keypad__key__value">3</p>
+            </div>
+            <div className="keypad__key--number">
+              <p className="keypad__key__value">2</p>
+            </div>
+            <div className="keypad__key--number">
+              <p className="keypad__key__value">1</p>
+            </div>
+            <div className="keypad__key--number">
+              <p className="keypad__key__value">.</p>
+            </div>
+            <div className="keypad__key--number">
+              <p className="keypad__key__value">0</p>
+            </div>
+            <div className="keypad__key--number">
+              <p className="keypad__key__value">ce</p>
+            </div>
+          </div>
+          <div className="keypad__operator">
+            <div className="keypad__key--operator">
+              <p className="keypad__key__value">/</p>
+            </div>
+            <div className="keypad__key--operator">
+              <p className="keypad__key__value">*</p>
+            </div>
+            <div className="keypad__key--operator">
+              <p className="keypad__key__value">+</p>
+            </div>
+            <div className="keypad__key--operator">
+              <p className="keypad__key__value">-</p>
+            </div>
+          </div>
+          <div className="keypad__submit">
+          <div className="keypad__key--submit">
+              <p className="keypad__key__value">=</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
